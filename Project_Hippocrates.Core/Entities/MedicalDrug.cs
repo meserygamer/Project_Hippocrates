bffd309a9a1067b87c76a207193fe8e02d06f066
MeseryGG <meserygamer@gmail.com>
2024-11-05 00:10:52 +0300

    Add MedicalDrug Entity

---
+++ Project_Hippocrates.Core/Entities/MedicalDrug.cs
@@ -0,0 +1,15 @@
+﻿namespace Project_Hippocrates.Core.Entities
+{
+    public class MedicalDrug
+    {
+        /// <summary>
+        /// Drug Id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Drug name
+        /// </summary>
+        public string Name { get; set; } = null!;
+    }
+}
