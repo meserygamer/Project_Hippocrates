ff117fb0ffd852eb2a74d12ee47c6c18b63b96fa
MeseryGG <meserygamer@gmail.com>
2024-11-05 00:55:25 +0300

    Add MedicationTime entity

---
+++ Project_Hippocrates.Core/Entities/MedicationTime.cs
@@ -0,0 +1,24 @@
+﻿namespace Project_Hippocrates.Core.Entities;
+
+public class MedicationTime
+{
+    /// <summary>
+    /// Medication time id
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Medication time label
+    /// </summary>
+    public string Label { get; set; } = null!;
+
+    /// <summary>
+    /// Time to take medication
+    /// </summary>
+    public TimeOnly Time { get; set; }
+
+    /// <summary>
+    /// Medications taken at this time
+    /// </summary>
+    public IEnumerable<DrugDosage> MedicationsTaken { get; set; } = null!;
+}
