89652de102650fade4df07b91b86a6317b748d51
MeseryGG <meserygamer@gmail.com>
2024-11-05 00:11:11 +0300

    Add MedicationSchedule Entity

---
+++ Project_Hippocrates.Core/Entities/MedicationSchedule.cs
@@ -0,0 +1,15 @@
+﻿namespace Project_Hippocrates.Core.Entities
+{
+    public class MedicationSchedule
+    {
+        /// <summary>
+        /// Medication schedule id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Medication schedule name
+        /// </summary>
+        public string Name { get; set; } = null!;
+    }
+}
