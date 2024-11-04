617325ce146396f3455f30352a5edf51bf764a6e
MeseryGG <meserygamer@gmail.com>
2024-11-05 00:54:49 +0300

    Add DrugDosage Entity

---
+++ Project_Hippocrates.Core/Entities/DrugDosage.cs
@@ -0,0 +1,24 @@
+﻿namespace Project_Hippocrates.Core.Entities;
+
+public class DrugDosage
+{
+    /// <summary>
+    /// Dosage id
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Drug that's taking
+    /// </summary>
+    public MedicalDrug Drug { get; set; } = null!;
+
+    /// <summary>
+    /// Drug dosage value
+    /// </summary>
+    public int DosageValue { get; set; }
+
+    /// <summary>
+    /// Unit of drug dosage value
+    /// </summary>
+    public string DosageUnit { get; set; } = null!;
+}
