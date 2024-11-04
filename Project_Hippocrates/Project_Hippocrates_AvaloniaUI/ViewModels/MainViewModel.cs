b916648e6df207fd5f7f6a03c1d36537125746ab
MeseryGG <meserygamer@gmail.com>
2024-11-05 00:05:27 +0300

    Add ItemRepeater

--- Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/MainViewModel.cs
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/MainViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace Project_Hippocrates_AvaloniaUI.ViewModels
 {
     public partial class MainViewModel : ViewModelBase
     {
         [ObservableProperty]
-        private string _greeting = "Welcome to Avalonia!";
+        private ObservableCollection<string> _testCollection = ["test1", "test2", "test3"];
     }
 }
