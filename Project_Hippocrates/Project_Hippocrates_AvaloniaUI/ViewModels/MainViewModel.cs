9b0426c04506a953ed9c0e3fd84e4daf2692f349
MeseryGG <meserygamer@gmail.com>
2024-11-05 14:31:12 +0300

    Add color resource file and style for MainView

--- Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/MainViewModel.cs
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Project_Hippocrates.Core.Entities;
 using System.Collections.ObjectModel;
 
 namespace Project_Hippocrates_AvaloniaUI.ViewModels
@@ -6,6 +7,11 @@ namespace Project_Hippocrates_AvaloniaUI.ViewModels
     public partial class MainViewModel : ViewModelBase
     {
         [ObservableProperty]
-        private ObservableCollection<string> _testCollection = ["test1", "test2", "test3"];
+        private ObservableCollection<MedicationTime> _testCollection = [
+            
+            new MedicationTime { Label = "Label1", Time = new System.TimeOnly(12, 0) },
+            new MedicationTime { Label = "Label2", Time = new System.TimeOnly(14, 0) },
+            new MedicationTime { Label = "Label3", Time = new System.TimeOnly(16, 0) },
+            ];
     }
 }
