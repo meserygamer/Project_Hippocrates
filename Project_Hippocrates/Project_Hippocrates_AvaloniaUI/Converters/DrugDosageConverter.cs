fed624418de42c5c5fc8c6834beabc83dc56d86c
MeseryGG <meserygamer@gmail.com>
2024-11-06 14:03:10 +0300

    Add DrugDosageValueConverter

---
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Converters/DrugDosageConverter.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia.Data.Converters;
+using Project_Hippocrates.Core.Entities;
+using System;
+using System.Globalization;
+
+namespace Project_Hippocrates_AvaloniaUI.Converters;
+
+public class DrugDosageConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null)
+            return string.Empty;
+
+        if (value is not DrugDosage)
+            throw new NotSupportedException("Value is not DrugDosage");
+
+        if (targetType != typeof(string))
+            throw new NotSupportedException("DrugDosageConverter not support this convertion type");
+
+        DrugDosage dosage = (DrugDosage)value;
+        return $"{dosage.Drug.Name} ({dosage.DosageValue} {dosage.DosageUnit})";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException("Convertor only for display data");
+    }
+}
--- Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/MainViewModel.cs
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/MainViewModel.cs
@@ -9,9 +9,16 @@ namespace Project_Hippocrates_AvaloniaUI.ViewModels
         [ObservableProperty]
         private ObservableCollection<MedicationTime> _testCollection = [
             
-            new MedicationTime { Label = "Label1", Time = new System.TimeOnly(12, 0) },
-            new MedicationTime { Label = "Label2", Time = new System.TimeOnly(14, 0) },
-            new MedicationTime { Label = "Label3", Time = new System.TimeOnly(16, 0) },
+            new MedicationTime { Label = "Label1", Time = new System.TimeOnly(12, 0), MedicationsTaken = _testCollection2 },
+            new MedicationTime { Label = "Label2", Time = new System.TimeOnly(14, 0), MedicationsTaken = _testCollection2 },
+            new MedicationTime { Label = "Label3", Time = new System.TimeOnly(16, 0), MedicationsTaken = _testCollection2 },
+            ];
+
+        private static ObservableCollection<DrugDosage> _testCollection2 = [
+
+            new DrugDosage() { Drug = new MedicalDrug() { Name = "Test Drug 1" }, DosageValue=1, DosageUnit="т." },
+            new DrugDosage() { Drug = new MedicalDrug() { Name = "Test Drug 2" }, DosageValue=12, DosageUnit="кап." },
+            new DrugDosage() { Drug = new MedicalDrug() { Name = "Test Drug 3" }, DosageValue=25, DosageUnit="мл." },
             ];
     }
 }
