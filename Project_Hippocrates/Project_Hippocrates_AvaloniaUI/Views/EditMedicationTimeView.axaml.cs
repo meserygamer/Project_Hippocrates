9e54fcfd317359896bb62bc7aca522887ea93386
MeseryGG <meserygamer@gmail.com>
2024-11-10 03:11:25 +0300

    Add EditMedicationTime UserControl as prototype

---
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Views/EditMedicationTimeView.axaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Project_Hippocrates_AvaloniaUI.Views;
+
+public partial class EditMedicationTimeView : UserControl
+{
+    public EditMedicationTimeView()
+    {
+        InitializeComponent();
+        DataGrid.ItemsSource = new List<Test>
+        {
+            new(){DrugName = "TestDrugName", DrugDose = "TestDrugDose"}
+        };
+    }
+
+    public class Test
+    {
+        public string DrugName { get; set; }
+        public string DrugDose { get; set; }
+    }
+}
