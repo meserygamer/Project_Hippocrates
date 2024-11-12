af6f1487b34b994d048634a303238862fcf5359e
MeseryGG <meserygamer@gmail.com>
2024-11-12 23:40:48 +0300

    Add IDomainEntityRepository into core

---
+++ Project_Hippocrates.Core/IDomainEntityRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Project_Hippocrates.Core;
+
+public interface IDomainEntityRepository<TEntity>
+{
+    IEnumerable<TEntity> GetAll();
+    TEntity GetById(Guid id);
+    TEntity Save(TEntity newValue);
+}
