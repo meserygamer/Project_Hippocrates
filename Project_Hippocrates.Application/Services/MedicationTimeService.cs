46bef27e1f9ae79ba5fa4c5be4faa50ffe8e8292
MeseryGG <meserygamer@gmail.com>
2024-11-13 14:41:42 +0300

    Add MedicationTimeService

---
+++ Project_Hippocrates.Application/Services/MedicationTimeService.cs
@@ -0,0 +1,24 @@
+﻿using Project_Hippocrates.Core;
+using Project_Hippocrates.Core.Entities;
+
+namespace Project_Hippocrates.Application.Services;
+
+public class MedicationTimeService
+{
+    private IDomainEntityRepository<MedicationTime> _repository;
+
+    public MedicationTimeService(IDomainEntityRepository<MedicationTime> repository)
+    {
+        _repository = repository;
+    }
+
+    public bool CreateMedicationTime(MedicationTime medicationTime)
+        => _repository.Add(medicationTime);
+    public bool SaveMedicationTimeChanges(MedicationTime medicationTime)
+        => _repository.Save(medicationTime);
+    
+    public Task<bool> CreateMedicationTimeAsync(MedicationTime medicationTime)
+        => _repository.AddAsync(medicationTime);
+    public Task<bool> SaveMedicationTimeChangesAsync(MedicationTime medicationTime)
+        => _repository.SaveAsync(medicationTime);
+}
