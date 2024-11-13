62d1728247a0eeb002c4b31ff7d00825ff3e3149
MeseryGG <meserygamer@gmail.com>
2024-11-13 16:02:51 +0300

    Add configures for mapster
    Add configures for types:
    - MedicationTimePresenter -> MedicationTime
    - DrugDosagePresenter -> DrugDosage

---
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Mapping/DrugDosageMappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using Mapster;
+using Project_Hippocrates_AvaloniaUI.Models.EntityPresenters;
+using Project_Hippocrates.Core.Entities;
+
+namespace Project_Hippocrates_AvaloniaUI.Mapping;
+
+public class DrugDosageMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<DrugDosagePresenter, DrugDosage>()
+            .BeforeMapping(dest => dest.Drug = new MedicalDrug())
+            .Map(dest => dest.Drug.Name, src => src.DrugName)
+            .Map(dest => dest.DrugDoseValue, src => src.DrugDoseValue)
+            .Map(dest => dest.DoseUnit, src => src.DoseUnit);
+    }
+}
---
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Mapping/MedicationTimeMappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using Mapster;
+using Project_Hippocrates_AvaloniaUI.Models.EntityPresenters;
+using Project_Hippocrates.Core.Entities;
+
+namespace Project_Hippocrates_AvaloniaUI.Mapping;
+
+public class MedicationTimeMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<MedicationTimePresenter, MedicationTime>()
+            .Map(dest => dest.Label, src => src.Label)
+            .Map(dest => dest.Time, src => src.Time)
+            .Map(dest => dest.MedicationsTaken, src => src.MedicationsTaken);
+    }
+}
