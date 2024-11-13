fbff51955c829a84d8dd775a34068d9c5994388b
MeseryGG <meserygamer@gmail.com>
2024-11-13 16:04:10 +0300

    Add realization for model's methods
    Add realization for:
    - TryCreateMedicationTimeModel in CreateMedicationTimeModel
    - TrySaveMedicationTimeChanges in EditExistingMedicationTimeModel

--- Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Models/EditMedicationTimeModels/CreateMedicationTimeModel.cs
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Models/EditMedicationTimeModels/CreateMedicationTimeModel.cs
@@ -1,18 +1,36 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using MapsterMapper;
 using Project_Hippocrates_AvaloniaUI.Models.EntityPresenters;
 using Project_Hippocrates_AvaloniaUI.ViewModels.EditMedicationTimeViewModels;
+using Project_Hippocrates.Application.Services;
+using Project_Hippocrates.Core.Entities;
 
 namespace Project_Hippocrates_AvaloniaUI.Models.EditMedicationTimeModels;
 
 public class CreateMedicationTimeModel : ModelBase<CreateMedicationTimeViewModel>
 {
-    public CreateMedicationTimeModel() : base() { }
-    public CreateMedicationTimeModel(CreateMedicationTimeViewModel vm) : base(vm) { }
-
-    public async Task<bool> TryCreateMedicationTimeModel(MedicationTimePresenter medicationTimePresenter)
+    private IMapper _mapper;
+    private MedicationTimeService _medicationTimeService;
+    
+    public CreateMedicationTimeModel(IMapper mapper,
+        MedicationTimeService medicationTimeService) 
+        : base()
     {
-        //TODO Create Method
-        throw new NotImplementedException();
+        _mapper = mapper;
+        _medicationTimeService = medicationTimeService;
+    }
+    public CreateMedicationTimeModel(CreateMedicationTimeViewModel vm,
+        IMapper mapper,
+        MedicationTimeService medicationTimeService) 
+        : base(vm)
+    {
+        _mapper = mapper;
+        _medicationTimeService = medicationTimeService;
+    }
+
+    public async Task<bool> TryCreateMedicationTimeModel(MedicationTimePresenter presenter)
+    {
+        MedicationTime medicationTime = _mapper.Map<MedicationTime>(presenter);
+        return await _medicationTimeService.CreateMedicationTimeAsync(medicationTime);
     }
 }
--- Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Models/EditMedicationTimeModels/EditExistingMedicationTimeModel.cs
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Models/EditMedicationTimeModels/EditExistingMedicationTimeModel.cs
@@ -1,18 +1,36 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using MapsterMapper;
 using Project_Hippocrates_AvaloniaUI.Models.EntityPresenters;
 using Project_Hippocrates_AvaloniaUI.ViewModels.EditMedicationTimeViewModels;
+using Project_Hippocrates.Application.Services;
+using Project_Hippocrates.Core.Entities;
 
 namespace Project_Hippocrates_AvaloniaUI.Models.EditMedicationTimeModels;
 
 public class EditExistingMedicationTimeModel : ModelBase<EditExistingMedicationTimeViewModel>
 {
-    public EditExistingMedicationTimeModel() : base() { }
-    public EditExistingMedicationTimeModel(EditExistingMedicationTimeViewModel vm) : base(vm) { }
+    private IMapper _mapper;
+    private MedicationTimeService _medicationTimeService;
+    
+    public EditExistingMedicationTimeModel(IMapper mapper,
+        MedicationTimeService medicationTimeService) 
+        : base()
+    {
+        _mapper = mapper;
+        _medicationTimeService = medicationTimeService;
+    }
+    public EditExistingMedicationTimeModel(EditExistingMedicationTimeViewModel vm,
+        IMapper mapper,
+        MedicationTimeService medicationTimeService) 
+        : base(vm)
+    {
+        _mapper = mapper;
+        _medicationTimeService = medicationTimeService;
+    }
 
     public async Task<bool> TrySaveMedicationTimeChanges(MedicationTimePresenter presenter)
     {
-        //TODO Create Method
-        throw new NotImplementedException();
+        MedicationTime medicationTime = _mapper.Map<MedicationTime>(presenter);
+        return await _medicationTimeService.SaveMedicationTimeChangesAsync(medicationTime);
     }
 }
