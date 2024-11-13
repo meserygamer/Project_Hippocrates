f076f27a562b3692fdd6cafc971d52251d905344
MeseryGG <meserygamer@gmail.com>
2024-11-13 14:41:06 +0300

    Add async version of methods into IDomainEntityRepository.cs

--- Project_Hippocrates.Core/IDomainEntityRepository.cs
+++ Project_Hippocrates.Core/IDomainEntityRepository.cs
@@ -4,5 +4,11 @@ public interface IDomainEntityRepository<TEntity>
 {
     IEnumerable<TEntity> GetAll();
     TEntity GetById(Guid id);
-    TEntity Save(TEntity newValue);
+    bool Add(TEntity entity);
+    bool Save(TEntity newValue);
+    
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<TEntity> GetByIdAsync(Guid id);
+    Task<bool> AddAsync(TEntity entity);
+    Task<bool> SaveAsync(TEntity newValue);
 }
