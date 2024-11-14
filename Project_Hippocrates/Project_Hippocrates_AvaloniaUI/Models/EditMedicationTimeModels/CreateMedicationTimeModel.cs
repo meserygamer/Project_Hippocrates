cb0e96955c071ccc0fd573fa3f2f090152e28b1b
MeseryGG <meserygamer@gmail.com>
2024-11-14 14:45:36 +0300

    Remove useless constructors from models
    Remove constructors with ViewModel from:
    - CreateMedicationTimeModel.cs
    - EditExistingMedicationTimeModel.cs

--- Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Models/EditMedicationTimeModels/CreateMedicationTimeModel.cs
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Models/EditMedicationTimeModels/CreateMedicationTimeModel.cs
@@ -1,7 +1,7 @@
 ﻿using System.Threading.Tasks;
 using MapsterMapper;
 using Project_Hippocrates_AvaloniaUI.Models.EntityPresenters;
-using Project_Hippocrates_AvaloniaUI.ViewModels.EditMedicationTimeViewModels;
+using Project_Hippocrates_AvaloniaUI.ViewModels;
 using Project_Hippocrates.Application.Services;
 using Project_Hippocrates.Core.Entities;
 
@@ -15,14 +15,6 @@ public class CreateMedicationTimeModel : ModelBase<CreateMedicationTimeViewModel
     public CreateMedicationTimeModel(IMapper mapper,
         MedicationTimeService medicationTimeService) 
         : base()
-    {
-        _mapper = mapper;
-        _medicationTimeService = medicationTimeService;
-    }
-    public CreateMedicationTimeModel(CreateMedicationTimeViewModel vm,
-        IMapper mapper,
-        MedicationTimeService medicationTimeService) 
-        : base(vm)
     {
         _mapper = mapper;
         _medicationTimeService = medicationTimeService;
