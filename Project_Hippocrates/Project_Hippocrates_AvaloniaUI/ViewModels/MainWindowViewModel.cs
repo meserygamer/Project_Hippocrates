8bac591a8af4b40da9bd9323eda5527515fc8d2f
MeseryGG <meserygamer@gmail.com>
2024-11-14 14:51:01 +0300

    Change MainWindowViewModel

---
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Project_Hippocrates_AvaloniaUI.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private Control? _showingView;
+    private ServiceProvider _serviceProvider;
+
+    public MainWindowViewModel()
+    {
+        _serviceProvider = App.Services;
+        CreateMedicationTimeViewModel? vm = _serviceProvider.GetService<CreateMedicationTimeViewModel>();
+        if (vm is null)
+            throw new ArgumentNullException();
+        ShowingView = new ViewLocator().Build(vm.ViewModelName);
+        ShowingView!.DataContext = vm;
+    }
+}
