baf3cfa3f2420287598a312772ff0ccb76f90a66
MeseryGG <meserygamer@gmail.com>
2024-11-16 23:23:42 +0300

    Add mapping config from MedicationTime to MedicationTimePresenter for MedicationTimeMappingConfig.cs

--- Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Mapping/MedicationTimeMappingConfig.cs
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Mapping/MedicationTimeMappingConfig.cs
@@ -1,4 +1,7 @@
-﻿using Mapster;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Mapster;
 using Project_Hippocrates_AvaloniaUI.Models.EntityPresenters;
 using Project_Hippocrates.Core.Entities;
 
@@ -13,5 +16,17 @@ public class MedicationTimeMappingConfig : IRegister
             .Map(dest => dest.Label, src => src.Label)
             .Map(dest => dest.Time, src => src.Time)
             .Map(dest => dest.MedicationsTaken, src => src.MedicationsTaken);
+        
+        config.NewConfig<MedicationTime, MedicationTimePresenter>()
+            .Map(dest => dest.DrugId, src => src.Id)
+            .Map(dest => dest.Label, src => src.Label)
+            .Map(dest => dest.Time, src => src.Time)
+            .Map(dest => dest.MedicationsTaken, src => ConvertMedicationTaken(src.MedicationsTaken));
+    }
+
+    private static ObservableCollection<DrugDosagePresenter> ConvertMedicationTaken(IEnumerable<DrugDosage> drugDosages)
+    {
+        IEnumerable<DrugDosagePresenter> presenter = drugDosages.Select((dosage, _) => dosage.Adapt<DrugDosagePresenter>());
+        return new ObservableCollection<DrugDosagePresenter>(presenter);
     }
 }
