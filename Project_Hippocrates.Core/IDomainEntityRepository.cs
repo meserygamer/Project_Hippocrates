1f506be45530389f3615a291aa42a3df75a8f366
MeseryGG <meserygamer@gmail.com>
2024-11-17 14:40:37 +0300

    Change return type in IDomainEntityRepository.cs GetByIdAsync and GetById

--- Project_Hippocrates.Core/IDomainEntityRepository.cs
+++ Project_Hippocrates.Core/IDomainEntityRepository.cs
@@ -3,12 +3,12 @@
 public interface IDomainEntityRepository<TEntity>
 {
     IEnumerable<TEntity> GetAll();
-    TEntity GetById(Guid id);
+    TEntity? GetById(Guid id);
     bool Add(TEntity entity);
     bool Save(TEntity newValue);
     
     Task<IEnumerable<TEntity>> GetAllAsync();
-    Task<TEntity> GetByIdAsync(Guid id);
+    Task<TEntity?> GetByIdAsync(Guid id);
     Task<bool> AddAsync(TEntity entity);
     Task<bool> SaveAsync(TEntity newValue);
 }
