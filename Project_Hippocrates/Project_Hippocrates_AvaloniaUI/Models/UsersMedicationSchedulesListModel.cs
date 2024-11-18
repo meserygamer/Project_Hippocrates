0fb7280dc819500cb961e3de4ee327d7da53f277
MeseryGG <meserygamer@gmail.com>
2024-11-19 00:02:21 +0300

    Rename MainView to UsersMedicationSchedulesListView

---
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/Models/UsersMedicationSchedulesListModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using MapsterMapper;
+using Project_Hippocrates_AvaloniaUI.Models.DTOs;
+using Project_Hippocrates_AvaloniaUI.ViewModels;
+using Project_Hippocrates.Application.Services;
+using Project_Hippocrates.Core.Entities;
+
+namespace Project_Hippocrates_AvaloniaUI.Models;
+
+public class UsersMedicationSchedulesListModel : ModelBase<UsersMedicationSchedulesListViewModel>
+{
+    private MedicationScheduleService _medicationScheduleService;
+    private IMapper _mapper;
+    
+    public UsersMedicationSchedulesListModel(MedicationScheduleService medicationScheduleService,
+        IMapper mapper)
+    {
+        _medicationScheduleService = medicationScheduleService;
+        _mapper = mapper;
+    }
+
+    public async Task<ObservableCollection<MedicationScheduleDTO>> GetAllMedicationScheduleAsync()
+    {
+        IEnumerable<MedicationSchedule> medicationSchedules = await _medicationScheduleService.GetAllMedicationSchedules();
+        IEnumerable<MedicationScheduleDTO> medicationSchedulesDtos =
+            _mapper.Map<IEnumerable<MedicationScheduleDTO>>(medicationSchedules);
+        return new ObservableCollection<MedicationScheduleDTO>(medicationSchedulesDtos);
+    }
+}
---
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/UsersMedicationSchedulesListViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Project_Hippocrates_AvaloniaUI.Models;
+using Project_Hippocrates_AvaloniaUI.Models.DTOs;
+
+namespace Project_Hippocrates_AvaloniaUI.ViewModels
+{
+    public class UsersMedicationSchedulesListViewModel : ViewModelBase
+    {
+        #region Fields
+
+        private readonly UsersMedicationSchedulesListModel _model;
+
+        private ObservableCollection<MedicationScheduleDTO> _medicationSchedules = [];
+        private ObservableCollection<MedicationTimeDTO>? _medicationTimes;
+        
+        private MedicationScheduleDTO? _selectedMedicationSchedule;
+
+        #endregion
+        
+        public UsersMedicationSchedulesListViewModel(UsersMedicationSchedulesListModel model)
+        {
+            _model = model;
+            _model.ViewModel = this;
+        }
+
+        #region Implementation ViewModelBase
+
+        public override async Task InitializeForShowAsync(Bundle? bundle)
+        {
+            MedicationSchedules = await _model.GetAllMedicationScheduleAsync();
+            SelectedMedicationSchedule = MedicationSchedules[0];
+        }
+
+        #endregion
+
+        #region Properties
+
+        public MedicationScheduleDTO? SelectedMedicationSchedule
+        {
+            get => _selectedMedicationSchedule;
+            set
+            {
+                SetProperty(ref _selectedMedicationSchedule, value);
+                OnSelectedMedicationScheduleChanged();
+            }
+        }
+        public ObservableCollection<MedicationScheduleDTO> MedicationSchedules
+        {
+            get => _medicationSchedules;
+            set
+            {
+                SetProperty(ref _medicationSchedules, value);
+                OnMedicationSchedulesChanged();
+            }
+        }
+        public ObservableCollection<MedicationTimeDTO> MedicationTimes
+        {
+            get => _medicationTimes ?? [];
+            set => SetProperty(ref _medicationTimes, value);
+        }
+
+        #endregion
+
+        #region Handlers
+
+        private void OnSelectedMedicationScheduleChanged()
+        {
+            MedicationTimes = SelectedMedicationSchedule?.MedicationTimes ?? [];
+        }
+        private void OnMedicationSchedulesChanged()
+        {
+            SelectedMedicationSchedule = null;
+        }
+
+        #endregion
+    }
+}
