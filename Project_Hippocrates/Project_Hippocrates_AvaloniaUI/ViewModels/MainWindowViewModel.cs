c3af9143fb29fa1cec61d8daf87aed42cd88fa46
MeseryGG <meserygamer@gmail.com>
2024-11-19 00:04:56 +0300

    Modify Method setup showing view

--- Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/MainWindowViewModel.cs
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Threading;
+using Project_Hippocrates.Core;
+using Project_Hippocrates.Core.Entities;
 
 namespace Project_Hippocrates_AvaloniaUI.ViewModels;
 
@@ -15,7 +21,9 @@ public class MainWindowViewModel : ViewModelBase
     {
         _serviceProvider = serviceProvider;
         _viewLocator = viewLocator;
-        SetShowingView(typeof(CreateMedicationTimeViewModel));
+        var medicationTimeRepo = (IDomainEntityRepository<MedicationTime>)_serviceProvider.GetService(typeof(IDomainEntityRepository<MedicationTime>))!;
+        var medicationTime = medicationTimeRepo.GetAll().First();
+        SetShowingView(typeof(EditExistingMedicationTimeViewModel), new Bundle(new Dictionary<string, object?> { { "ChangingMedicationTimeId", medicationTime.Id } }));
     }
 
     public Control? ShowingView
@@ -24,10 +32,26 @@ public class MainWindowViewModel : ViewModelBase
         private set => SetProperty(ref _showingView, value, nameof(ShowingView));
     }
 
-    public void SetShowingView(Type viewModelType)
+    public void SetShowingView(Type viewModelType, Bundle? bundle)
     {
-        var vm = _serviceProvider.GetService(viewModelType) 
-                                 ?? throw new ArgumentOutOfRangeException(nameof(viewModelType), "not compatible viewModel type");
+        Task.Run(async () =>
+        {
+            ViewModelBase vm = (ViewModelBase?)_serviceProvider.GetService(viewModelType)
+                               ?? throw new ArgumentOutOfRangeException(nameof(viewModelType),
+                                   "not compatible viewModel type");
+            await vm.InitializeForShowAsync(bundle);
+            Dispatcher.UIThread.InvokeAsync(async () =>
+            {
+                ShowingView = _viewLocator.BuildViewWithDataContext((ViewModelBase)vm);
+            });
+        });
+    }
+    
+    public async Task SetShowingViewAsync(Type viewModelType, Bundle? bundle)
+    {
+        ViewModelBase vm = (ViewModelBase?)_serviceProvider.GetService(viewModelType)
+                           ?? throw new ArgumentOutOfRangeException(nameof(viewModelType), "not compatible viewModel type");
+        await vm.InitializeForShowAsync(bundle);
         ShowingView = _viewLocator.BuildViewWithDataContext((ViewModelBase)vm);
     }
 }
