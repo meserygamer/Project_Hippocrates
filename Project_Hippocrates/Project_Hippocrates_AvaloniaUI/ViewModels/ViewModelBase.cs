51d008de006398b0d9dca89258551a298c9b062d
MeseryGG <meserygamer@gmail.com>
2024-11-19 00:01:42 +0300

    Add InitializeForShowAsync for initialize view data

--- Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/ViewModelBase.cs
+++ Project_Hippocrates/Project_Hippocrates_AvaloniaUI/ViewModels/ViewModelBase.cs
@@ -1,9 +1,12 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Project_Hippocrates_AvaloniaUI.ViewModels
 {
     public abstract class ViewModelBase : ObservableObject
     {
         public virtual string? ViewModelFullName => GetType().FullName;
+        
+        public virtual Task InitializeForShowAsync(Bundle? bundle) => Task.CompletedTask;
     }
 }
