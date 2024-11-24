b2b2afacf9aa3b97af641b16409057d3ca6ac144
MeseryGG <meserygamer@gmail.com>
2024-11-24 16:22:33 +0300

    Add SQLiteDbContext and ISQLiteDbConnectionStringProvider

---
+++ Project_Hippocrates.SQLite/SqLiteDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_Hippocrates.SQLite.SqlEntities;
+
+namespace Project_Hippocrates.SQLite;
+
+public class SqLiteDbContext : DbContext
+{
+    private readonly ISqLiteDbConnectionStringProvider _connectionStringProvider;
+
+    public SqLiteDbContext(ISqLiteDbConnectionStringProvider connectionStringProvider)
+    {
+        _connectionStringProvider = connectionStringProvider;
+    }
+
+    public DbSet<DrugDosageEntity> DrugDosages { get; set; }
+    public DbSet<MedicalDrugEntity> MedicalDrugs { get; set; }
+    public DbSet<MedicationScheduleEntity> MedicationSchedules { get; set; }
+    public DbSet<MedicationTimeEntity> MedicationTimes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(_connectionStringProvider.ConnectionString);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SqLiteDbContext).Assembly);
+    }
+}
