373e91238b22c87e09e4638ffaa20fa2deeb4fbe
MeseryGG <meserygamer@gmail.com>
2024-11-26 23:54:15 +0300

    Add mappers for SQLiteDb

---
+++ Project_Hippocrates.SQLite/Mapping/MedicalDrugEntityMappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using Mapster;
+using Project_Hippocrates.Core.Entities;
+using Project_Hippocrates.SQLite.SqlEntities;
+
+namespace Project_Hippocrates.SQLite.Mapping;
+
+public class MedicalDrugEntityMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<MedicalDrugEntity, MedicalDrug>()
+            .Map(dest => dest.Id, source => source.Id)
+            .Map(dest => dest.Name, source => source.Name);
+        
+        config.NewConfig<MedicalDrug, MedicalDrugEntity>()
+            .Map(dest => dest.Id, source => source.Id)
+            .Map(dest => dest.Name, source => source.Name);
+    }
+}
---
+++ Project_Hippocrates.SQLite/Mapping/MedicationTimeEntityMappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using Mapster;
+using Project_Hippocrates.Core.Entities;
+using Project_Hippocrates.SQLite.SqlEntities;
+
+namespace Project_Hippocrates.SQLite.Mapping;
+
+public class MedicationTimeEntityMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<MedicationTimeEntity, MedicationTime>()
+            .Map(dest => dest.Id, source => source.Id)
+            .Map(dest => dest.Label, source => source.Label)
+            .Map(dest => dest.Time, source => source.Time)
+            .Map(dest => dest.MedicationsTaken, source => source.MedicationsTaken.AsQueryable().ProjectToType<DrugDosage>(config));
+        
+        config.NewConfig<MedicationTime, MedicationTimeEntity>()
+            .Map(dest => dest.Id, source => source.Id)
+            .Map(dest => dest.Label, source => source.Label)
+            .Map(dest => dest.Time, source => source.Time)
+            .Map(dest => dest.MedicationsTaken, source => source.MedicationsTaken.AsQueryable().ProjectToType<DrugDosage>(config).ToList());
+    }
+}
