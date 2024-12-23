11143263bd859b82179d54876809bd72d0fab80e
MeseryGG <meserygamer@gmail.com>
2024-12-23 16:11:28 +0300

    Change MedicalDrug.DrugId to require(false)

---
+++ Project_Hippocrates.SQLite/Migrations/20241216191918_v1_3.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project_Hippocrates.SQLite.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DrugDosages_MedicalDrugs_DrugId",
+                table: "DrugDosages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DrugDosages_MedicalDrugs_DrugId",
+                table: "DrugDosages",
+                column: "DrugId",
+                principalTable: "MedicalDrugs",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DrugDosages_MedicalDrugs_DrugId",
+                table: "DrugDosages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DrugDosages_MedicalDrugs_DrugId",
+                table: "DrugDosages",
+                column: "DrugId",
+                principalTable: "MedicalDrugs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
