d696b573e7e5a36c80720550365403b3236bc1ab
MeseryGG <meserygamer@gmail.com>
2024-12-23 16:14:50 +0300

    Add IMedicationTimeStorageService and SQLite implementation

---
+++ Project_Hippocrates.Core/Interfaces/Storage/IMedicationTimeStorageService.cs
@@ -0,0 +1,8 @@
+﻿using Project_Hippocrates.Core.Entities;
+
+namespace Project_Hippocrates.Core.Interfaces.Storage;
+
+public interface IMedicationTimeStorageService
+{
+    Task<bool> CreateAndJoinToScheduleAsync(Guid scheduleId, MedicationTime medicationTime);
+}
